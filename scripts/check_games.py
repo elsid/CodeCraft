a9f4a029e443bc497ff37166096e82b2e0a4e3d2
elsid <elsid.mail@gmail.com>
2020-12-01 23:31:51 +0100

    Add script to check games

---
+++ scripts/check_games.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os.path
+import sys
+import results_stats
+
+
+def main():
+    paths = sorted(sys.argv[2:], key=get_time)
+    games = list(results_stats.collect_data(paths))
+    check_games(games, sys.argv[1])
+
+
+def check_games(games, player):
+    for game in games:
+        check_game(game, player)
+
+
+def check_game(game, player):
+    seed = game['seed']
+    results = game['results']
+    if sum(1 for v in results.values() if v['score'] == 0) == len(results):
+        print('zero_draw', seed, results[player]['position'])
+    scores = sorted((v['score'] for v in results.values()), reverse=True)
+    place = next((n for n, v in enumerate(scores) if v == results[player]['score'])) + 1
+    if place != 1:
+        print('loss', seed, place, results[player]['position'], results[player]['score'])
+    for k, v in results.items():
+        if v['crashed']:
+            print('crashed', k, seed, place, results[player]['position'], results[player]['score'])
+
+
+def get_time(path):
+    return int(os.path.basename(path).split('.')[-1])
+
+
+if __name__ == '__main__':
+    main()
