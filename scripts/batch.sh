1cbb582cbbbcaa77f0da82be53a385c0d016c061
elsid <elsid.mail@gmail.com>
2020-12-06 22:41:31 +0100

    Add scripts to run game in batch mode

---
+++ scripts/batch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+if ! [[ "${CONFIG}" ]]; then
+    CONFIG=${1:?}
+fi
+
+SRC="${PWD}"
+
+if ! [[ "${RUNNER}" ]]; then
+  if [[ ${OS} == "Windows_NT" ]]; then
+    RUNNER=aicup2020-windows
+  else
+    RUNNER=aicup2020-linux
+  fi
+fi
+
+cd cgdk/runners/${RUNNER}/
+
+ID=$(date +%Y-%m-%d_%H-%M-%S)
+
+mkdir -p ${SRC}/results/temp
+
+./aicup2020 \
+    --batch-mode \
+    --config ${SRC}/etc/${CONFIG:?}.json \
+    --save-results ${SRC}/results/temp/${ID}.results.json
