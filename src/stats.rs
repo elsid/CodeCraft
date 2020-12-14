bad30fa3c70d343ae529bb2620ab0f9702a20b31
elsid <elsid.mail@gmail.com>
2020-12-14 19:58:21 +0100

    Find path to hidden target

--- src/stats.rs
+++ src/stats.rs
@@ -5,27 +5,35 @@ use serde::Serialize;
 #[cfg_attr(feature = "write_stats", derive(Serialize))]
 pub struct StatsResult {
     pub entity_planner_iterations: usize,
+    pub find_hidden_path_calls: usize,
 }
 
 #[derive(Default)]
 pub struct Stats {
     entity_planner_iterations: usize,
+    find_hidden_path_calls: usize,
 }
 
 impl Stats {
     pub fn new() -> Self {
         Self {
             entity_planner_iterations: 0,
+            find_hidden_path_calls: 0,
         }
     }
 
     pub fn get_result(&self) -> StatsResult {
         StatsResult {
             entity_planner_iterations: self.entity_planner_iterations,
+            find_hidden_path_calls: self.find_hidden_path_calls,
         }
     }
 
     pub fn add_entity_planner_iterations(&mut self, number: usize) {
         self.entity_planner_iterations += number;
+    }
+
+    pub fn add_find_hidden_path_calls(&mut self, number: usize) {
+        self.find_hidden_path_calls += number;
     }
 }
