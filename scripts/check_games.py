d4e812944180d6d46555f2f150c92cf69007e1b6
elsid <elsid.mail@gmail.com>
2020-12-21 21:10:34 +0100

    Check for zero score games

--- scripts/check_games.py
+++ scripts/check_games.py
@@ -17,10 +17,12 @@ def check_games(games, player):
 
 
 def check_game(game, player):
-    seed = game['seed']
     results = game['results']
+    if player not in results.keys():
+        return
+    seed = game['seed']
     if sum(1 for v in results.values() if v['score'] == 0) == len(results):
-        print('zero_draw', seed, results[player]['position'])
+        print('zero_draw', seed, results[player]['position'], results[player]['score'])
     scores = sorted((v['score'] for v in results.values()), reverse=True)
     place = next((n for n, v in enumerate(scores) if v == results[player]['score'])) + 1
     if place != 1:
@@ -28,6 +30,8 @@ def check_game(game, player):
     for k, v in results.items():
         if v['crashed']:
             print('crashed', k, seed, place, v['position'], v['score'])
+    if results[player]['score'] == 0:
+        print('zero_score', seed, results[player]['position'], results[player]['score'])
 
 
 def get_time(path):
