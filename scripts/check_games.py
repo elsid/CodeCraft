e674dce2b908926d8c41558c12224d41d98024ba
elsid <elsid.mail@gmail.com>
2020-12-24 17:07:55 +0100

    Check games for absent player

--- scripts/check_games.py
+++ scripts/check_games.py
@@ -7,7 +7,7 @@ import results_stats
 
 def main():
     paths = sorted(sys.argv[2:], key=get_time)
-    games = list(results_stats.collect_data(paths))
+    games = results_stats.collect_data(paths)
     check_games(games, sys.argv[1])
 
 
@@ -18,20 +18,20 @@ def check_games(games, player):
 
 def check_game(game, player):
     results = game['results']
-    if player not in results.keys():
-        return
     seed = game['seed']
-    if sum(1 for v in results.values() if v['score'] == 0) == len(results):
-        print('zero_draw', seed, results[player]['position'], results[player]['score'])
-    scores = sorted((v['score'] for v in results.values()), reverse=True)
-    place = next((n for n, v in enumerate(scores) if v == results[player]['score'])) + 1
-    if place != 1:
-        print('loss', seed, place, results[player]['position'], results[player]['score'])
+    if player in results:
+        if sum(1 for v in results.values() if v['score'] == 0) == len(results):
+            print('zero_draw', seed, results[player]['position'], results[player]['score'])
+        scores = sorted((v['score'] for v in results.values()), reverse=True)
+        place = next((n for n, v in enumerate(scores) if v == results[player]['score'])) + 1
+        if place != 1:
+            print('loss', seed, place, results[player]['position'], results[player]['score'])
     for k, v in results.items():
         if v['crashed']:
-            print('crashed', k, seed, place, v['position'], v['score'])
-    if results[player]['score'] == 0:
-        print('zero_score', seed, results[player]['position'], results[player]['score'])
+            print('crashed', k, seed, v['position'], v['score'])
+    for k, v in results.items():
+        if v['score'] == 0:
+            print('zero_score', k, seed, v['position'], v['score'])
 
 
 def get_time(path):
